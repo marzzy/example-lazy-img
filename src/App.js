93f6337885c2fe3f556b2bcf1ac062bf1e3d3880
marzzy <marzzy.m@gmail.com>
2020-02-08 17:35:55 +0330

    feat: add samples

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import { example } from './sample';
+import LazyImage from './ImgComponent';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul className="App">
+      {example.map(item => (
+        <li key={item.id} className="ImgContainer">
+          <LazyImage src={item.url} alt={item.title} />
+        </li>
+      ))}
+    </ul>
   );
 }
 
